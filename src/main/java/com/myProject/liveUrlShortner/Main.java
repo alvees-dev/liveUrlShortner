6cdf524f277cfe5ae327678238a9136ae50185d4
Gabriel Alves <alvesg.dev@gmail.com>
2024-11-22 15:09:32 -0300

    HandleResquest method, add dependencies, build

--- src/main/java/com/myProject/liveUrlShortner/Main.java
+++ src/main/java/com/myProject/liveUrlShortner/Main.java
@@ -1,8 +1,40 @@
 package com.myProject.liveUrlShortner;
 
-public class Main {
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.logging.log4j.core.jackson.Log4jJsonObjectMapper;
 
-    public static void main(String[] args) {
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
 
+public class Main implements RequestHandler<Map<String, Object>, Map<String, String>> {
+
+    private  final ObjectMapper objectMapper = new Log4jJsonObjectMapper();
+
+    @Override
+    public Map<String, String> handleRequest(Map<String, Object> input, Context context) {
+        String body = (String) input.get("body").toString();
+
+        Map<String, String> bodyMap;
+
+        try{
+            bodyMap = objectMapper.readValue(body, Map.class);
+
+        } catch(JsonProcessingException exception) {
+            throw new RuntimeException("Error parsin JSON body" + exception.getMessage(), exception);
         }
+
+        String originalUrl = bodyMap.get("originalUrl");
+        String expirationTime = bodyMap.get("expirationTime");
+
+        String shortUrlCode = UUID.randomUUID().toString().substring(0, 8);
+
+        Map<String, String> response = new HashMap<>();
+        response.put("code", shortUrlCode);
+
+        return response;
     }
+}
